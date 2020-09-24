d8c4322effd35d498532b09f934e89453a64d767
Tim Howard <tim.guy.howard@gmail.com>
2020-09-24 07:00:48 -0500

    added Login and other basic pages

--- opengeo/opengeo/ClientApp/src/app/fuse-configs/routesConfig.js
+++ opengeo/opengeo/ClientApp/src/app/fuse-configs/routesConfig.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import FuseUtils from '@fuse/utils';
-import ExampleConfig from 'app/main/example/ExampleConfig';
-import OpenGeoMapPage from 'app/main/opengeo-map/OpenGeoMapPage';
 
-const routeConfigs = [ExampleConfig];
+import ForgotPassword2PageConfig from 'app/main/pages/auth/forgot-password-2/ForgotPassword2PageConfig';
+import ForgotPasswordPageConfig from 'app/main/pages//auth/forgot-password/ForgotPasswordPageConfig';
+import LockPageConfig from 'app/main/pages/auth/lock/LockPageConfig';
+import Login2PageConfig from 'app/main/pages/auth/login-2/Login2PageConfig';
+import LoginPageConfig from 'app/main/pages/auth/login/LoginPageConfig';
+import MailConfirmPageConfig from 'app/main/pages/auth/mail-confirm/MailConfirmPageConfig';
+import Register2PageConfig from 'app/main/pages/auth/register-2/Register2PageConfig';
+import RegisterPageConfig from 'app/main/pages/auth/register/RegisterPageConfig';
+import ResetPassword2PageConfig from 'app/main/pages/auth/reset-password-2/ResetPassword2PageConfig';
+import ResetPasswordPageConfig from 'app/main/pages/auth/reset-password/ResetPasswordPageConfig';
+import ComingSoonPageConfig from 'app/main/pages/coming-soon/ComingSoonPageConfig';
+import Error404PageConfig from 'app/main/pages/errors/404/Error404PageConfig';
+import Error500PageConfig from 'app/main/pages/errors/500/Error500PageConfig';
+import ProfilePageConfig from 'app/main/pages/profile/ProfilePageConfig';
+import MaintenancePageConfig from 'app/main/pages/maintenance/MaintenancePageConfig';
+
+import ExampleConfig from 'app/main/example/ExampleConfig';
+import OpenGeoMapPageConfig from 'app/main/opengeo-map/OpenGeoMapPageConfig';
+
+const routeConfigs = [
+	ForgotPassword2PageConfig,
+	ForgotPasswordPageConfig,
+	LockPageConfig,
+	Login2PageConfig,
+	LoginPageConfig,
+	MailConfirmPageConfig,
+	Register2PageConfig,
+	RegisterPageConfig,
+	ResetPassword2PageConfig,
+	ResetPasswordPageConfig,
+	ComingSoonPageConfig,
+	Error404PageConfig,
+	Error500PageConfig,
+	ProfilePageConfig,
+	MaintenancePageConfig,
+
+	ExampleConfig,
+	OpenGeoMapPageConfig
+
+
+];
 
 const routes = [
 	...FuseUtils.generateRoutesFromConfigs(routeConfigs),
-	{
-		path: '/opengeo-map/:id',
-		component: OpenGeoMapPage
-	},
 	{
 		path: '/',
 		component: () => <Redirect to="/example" />
---
+++ opengeo/opengeo/ClientApp/src/app/main/opengeo-map/OpenGeoMapPageConfig.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import OpenGeoMapPage from './OpenGeoMapPage';
+
+const OpenGeoMapPageConfig = {
+	settings: {
+		layout: {
+			config: {}
+		}
+	},
+	routes: [
+		{
+			path: '/opengeo-map/:id',
+			component: OpenGeoMapPage
+		}
+	]
+};
+
+export default OpenGeoMapPageConfig;
